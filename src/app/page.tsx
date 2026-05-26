781517d8e36e08f28a18ba8e475bdb134279aa6f
moukpu <aldiyar@moukpu.com>
2026-05-26 22:22:36 +0500

    auto-crop and remove backgrounds to make sakura and swallow transparent PNGs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,24 +21,24 @@ export default function Home() {
       {/* Hero Section Container */}
       <section className="relative w-full min-h-screen flex flex-col items-center justify-start pt-32 px-6 text-center z-10">
         
-        {/* Sakura Branch - positioned in the upper-left, extending horizontally with mix-blend-mode to multiply onto watercolor background */}
-        <div className="absolute left-0 top-0 w-[80%] max-w-[340px] pointer-events-none z-20 mix-blend-multiply select-none">
+        {/* Sakura Branch - positioned in the upper-left, extending horizontally, transparent background */}
+        <div className="absolute left-0 top-0 w-[80%] max-w-[340px] pointer-events-none z-20 select-none">
           <Image
             src="/images/sakura-top.png"
             alt="Sakura branch"
-            width={1024}
-            height={768}
+            width={839}
+            height={839}
             className="h-auto w-full object-contain origin-top-left"
             priority
           />
         </div>
 
-        {/* Flying Swallow Bird - floating on the right side of the screen facing the sakura branch */}
+        {/* Flying Swallow Bird - floating on the right side, transparent background */}
         <motion.div
           initial={{ opacity: 0, x: 50, y: -20 }}
           animate={{ opacity: 1, x: 0, y: 0 }}
           transition={{ duration: 1.2, ease: "easeOut", delay: 0.5 }}
-          className="absolute right-8 top-32 w-[90px] pointer-events-none z-20 mix-blend-multiply select-none"
+          className="absolute right-8 top-32 w-[90px] pointer-events-none z-20 select-none"
         >
           <motion.div
             animate={{ 
@@ -55,8 +55,8 @@ export default function Home() {
             <Image
               src="/images/swallow.png"
               alt="Flying swallow bird"
-              width={500}
-              height={500}
+              width={901}
+              height={901}
               className="h-auto w-full object-contain"
               priority
             />
@@ -65,7 +65,7 @@ export default function Home() {
 
         {/* Content Wrapper */}
         <div className="relative mt-auto mb-24 z-30 flex flex-col items-center">
-          {/* We will build the names and main titles in this block next */}
+          {/* Content will go here */}
         </div>
 
       </section>
