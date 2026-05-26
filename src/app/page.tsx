1fda5ed7a8a8c910f90e94a3009fb99b225195aa
moukpu <aldiyar@moukpu.com>
2026-05-27 00:10:58 +0500

    style(layout): increase vertical spacing of names to fully expose background ampersand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
               initial={{ opacity: 0, y: 15 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.2 }}
-              className="font-cursive text-[76px] text-dark leading-none -translate-x-[45px] -translate-y-2"
+              className="font-cursive text-[76px] text-dark leading-none -translate-x-[45px] -translate-y-10"
             >
               Ринат
             </motion.span>
@@ -88,7 +88,7 @@ export default function Home() {
               initial={{ opacity: 0, y: 15 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.5 }}
-              className="font-cursive text-[76px] text-dark leading-none translate-x-[45px] mt-12"
+              className="font-cursive text-[76px] text-dark leading-none translate-x-[45px] mt-24"
             >
               Динара
             </motion.span>
