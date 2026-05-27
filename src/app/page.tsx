8dc01af7195e64f3d701c4534e832ea94b0245d4
moukpu <aldiyar@moukpu.com>
2026-05-27 11:52:34 +0500

    Новый журавль и сакура-ветка, layout в потоке

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,50 +97,53 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Bottom Sakura Branch - right side, mirroring the top */}
-          <motion.div
-            initial={{ opacity: 0, x: 40 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.4, ease: "easeOut", delay: 0.8 }}
-            className="absolute right-0 bottom-[80px] w-[75%] max-w-[320px] pointer-events-none z-20 select-none"
-          >
-            <Image
-              src="/images/sakura-bottom.png"
-              alt="Sakura branch bottom"
-              width={950}
-              height={790}
-              className="h-auto w-full object-contain origin-bottom-right scale-x-[-1]"
-            />
-          </motion.div>
-
-          {/* Crane Bird - flying near the bottom sakura */}
-          <motion.div
-            initial={{ opacity: 0, x: -40, y: 20 }}
-            animate={{ opacity: 1, x: 0, y: 0 }}
-            transition={{ duration: 1.4, ease: "easeOut", delay: 1.2 }}
-            className="absolute left-[10px] bottom-[160px] w-[110px] pointer-events-none z-20 select-none"
-          >
+          {/* Bottom Decorations - sakura branch from right, crane flying left */}
+          <div className="relative w-full h-[300px] mt-8">
+            {/* Bottom Sakura Branch - extending from right side */}
             <motion.div
-              animate={{ 
-                y: [0, -6, 0],
-                x: [0, 4, 0],
-                rotate: [0, 3, 0]
-              }}
-              transition={{ 
-                duration: 6, 
-                repeat: Infinity, 
-                ease: "easeInOut" 
-              }}
+              initial={{ opacity: 0, x: 60 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.4, ease: "easeOut", delay: 0.8 }}
+              className="absolute right-0 top-0 w-[80%] max-w-[340px] pointer-events-none z-20 select-none"
             >
               <Image
-                src="/images/crane.png"
-                alt="Flying crane bird"
-                width={893}
-                height={713}
-                className="h-auto w-full object-contain scale-x-[-1]"
+                src="/images/sakura-bottom.png"
+                alt="Sakura branch bottom"
+                width={1024}
+                height={764}
+                className="h-auto w-full object-contain origin-top-right"
               />
             </motion.div>
-          </motion.div>
+
+            {/* Crane Bird - flying near the bottom sakura */}
+            <motion.div
+              initial={{ opacity: 0, x: -50, y: 30 }}
+              animate={{ opacity: 1, x: 0, y: 0 }}
+              transition={{ duration: 1.4, ease: "easeOut", delay: 1.2 }}
+              className="absolute left-[15px] top-[40px] w-[120px] pointer-events-none z-20 select-none"
+            >
+              <motion.div
+                animate={{ 
+                  y: [0, -7, 0],
+                  x: [0, 5, 0],
+                  rotate: [0, 3, 0]
+                }}
+                transition={{ 
+                  duration: 6, 
+                  repeat: Infinity, 
+                  ease: "easeInOut" 
+                }}
+              >
+                <Image
+                  src="/images/crane.png"
+                  alt="Flying crane bird"
+                  width={990}
+                  height={785}
+                  className="h-auto w-full object-contain"
+                />
+              </motion.div>
+            </motion.div>
+          </div>
 
         </section>
       </main>
